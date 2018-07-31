306b4fbe7c6464873263072a196e49391433e5e4
Pankaj Rai <pankaj.rai16@gmail.com>
2018-07-31 23:36:02 +0530

    Updated with Dart2

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() => runApp(new MyEntryWid());
+void main() => runApp(MyEntryWid());
 
 class MyEntryWid extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My material app",
-        home: new FeedbackTitle());
-  }
+  Widget build(BuildContext context) => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "My material app",
+      home: FeedbackTitle());
 }
 
 class FeedbackTitle extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _FeedbackState();
-  }
+  State<StatefulWidget> createState() => _FeedbackState();
 }
 
 class _FeedbackState extends State<FeedbackTitle> {
@@ -33,33 +29,29 @@ class _FeedbackState extends State<FeedbackTitle> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("FMS", style: new TextStyle(color: Colors.white)),
-          backgroundColor: Colors.blue,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(48.0),
-          child: Form(
-            key: formKey,
-            child: new ListView(
-              children: <Widget>[
-                _FeedbackTitleWidget(feedbackController),
-                Padding(
-                    padding: EdgeInsets.only(top: 24.0),
-                    child: _FeedbackWidget(feedbackTextController)),
-                Padding(
-                    padding: EdgeInsets.only(top: 24.0),
-                    child:
-                    _SubmitButton(
-                        feedbackController, feedbackTextController, formKey)
-                )
-              ],
-            ),
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text("FMS", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(48.0),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: <Widget>[
+              _FeedbackTitleWidget(feedbackController),
+              Padding(
+                  padding: EdgeInsets.only(top: 24.0),
+                  child: _FeedbackWidget(feedbackTextController)),
+              Padding(
+                  padding: EdgeInsets.only(top: 24.0),
+                  child: _SubmitButton(
+                      feedbackController, feedbackTextController, formKey))
+            ],
           ),
-        ));
-  }
+        ),
+      ));
 }
 
 class _FeedbackTitleWidget extends StatelessWidget {
@@ -68,45 +60,33 @@ class _FeedbackTitleWidget extends StatelessWidget {
   _FeedbackTitleWidget(this.feedbackController);
 
   @override
-  Widget build(BuildContext context) {
-    return new TextFormField(
-      decoration: InputDecoration(
-          border: OutlineInputBorder(), labelText: "Subject"),
-      controller: feedbackController,
-      validator: (val) =>
-      val.isEmpty ? "Please enter subject" : null,
-    );
-  }
+  Widget build(BuildContext context) => TextFormField(
+        decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Subject"),
+        controller: feedbackController,
+        validator: (val) => val.isEmpty ? "Please enter subject" : null,
+      );
 }
 
 class _FeedbackWidget extends StatelessWidget {
-
   final TextEditingController feedbackTextController;
 
   _FeedbackWidget(this.feedbackTextController);
 
   @override
-  Widget build(BuildContext context) {
-    return new TextFormField(
-      decoration: InputDecoration(
-          border: OutlineInputBorder(), labelText: "Feedback"),
-      keyboardType: TextInputType.multiline,
-      maxLines: 10,
+  Widget build(BuildContext context) => TextFormField(
+      decoration: InputDecoration(border: OutlineInputBorder(), labelText: "Feedback"),
+      keyboardType: TextInputType.multiline, maxLines: 10,
       controller: feedbackTextController,
-      validator: (val) =>
-      val.isEmpty ? "Please enter feedback" : null,
+      validator: (val) => val.isEmpty ? "Please enter feedback" : null,
     );
-  }
 }
 
 class _SubmitButton extends StatelessWidget {
-
   final TextEditingController _feedbackController;
   final TextEditingController _feedbackTextController;
   final GlobalKey<FormState> formKey;
 
-  _SubmitButton(this._feedbackController, this._feedbackTextController,
-      this.formKey);
+  _SubmitButton(this._feedbackController, this._feedbackTextController, this.formKey);
 
   @override
   Widget build(BuildContext context) {
@@ -115,23 +95,16 @@ class _SubmitButton extends StatelessWidget {
         child: new RaisedButton(
             padding: EdgeInsets.all(12.0),
             color: Colors.blue,
-            shape:
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-            child: const Text(
-              "Submit",
-              style: TextStyle(color: Colors.white, fontSize: 20.0),
-            ),
-            onPressed: () =>
-                _onSubmitPress(
-                    context, _feedbackController, _feedbackTextController,
-                    formKey)
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+            child: Text("Submit", style: TextStyle(color: Colors.white, fontSize: 20.0),),
+            onPressed: () => _onSubmitPress(context, _feedbackController, _feedbackTextController, formKey)
         ));
-  }}
+  }
+}
 
-void _onSubmitPress(BuildContext context,
-    TextEditingController feedbackController,
-    TextEditingController feedbackTextController,
-    GlobalKey<FormState> formKey) {
+void _onSubmitPress(BuildContext context, TextEditingController feedbackController,
+    TextEditingController feedbackTextController, GlobalKey<FormState> formKey) {
+
   var form = formKey.currentState;
   if (form.validate()) {
     form.save();
@@ -141,16 +114,14 @@ void _onSubmitPress(BuildContext context,
       'feedback': feedbackTextController.text
     }).whenComplete(() {
       print("Doc added");
-      Scaffold.of(context).showSnackBar(new SnackBar(
-          duration: new Duration(seconds: 4),
-          content: const Text("Feedback sumbitted successfully")));
+      Scaffold.of(context).showSnackBar(SnackBar(
+          duration: Duration(seconds: 4),
+          content: Text("Feedback sumbitted successfully")));
       feedbackController.clear();
       feedbackTextController.clear();
-    }).catchError(
-            (e) =>
-            Scaffold.of(context).showSnackBar(new SnackBar(
-              content: new Text(e.toString()),
-              duration: new Duration(seconds: 6),
-            )));
+    }).catchError((e) => Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text(e.toString()),
+          duration: Duration(seconds: 6),
+        )));
   }
 }
